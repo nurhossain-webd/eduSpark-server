b3e11c715cf111eec7c48e6a493df8cdc83b549d
Md Nur Hossain Riyad <md_nur_hossain.riyad@smail.th-koeln.de>
2026-07-14 21:02:06 +0600

    Complete authentication and admin authorization system

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,15 +1,21 @@
 import mongoose from "mongoose";
-import { env } from "./env";
 
 export default async function connectDB(): Promise<void> {
+  const mongodbUri = process.env.MONGODB_URI;
+
+  if (!mongodbUri) {
+    throw new Error("MONGODB_URI is not defined in .env");
+  }
+
   try {
-    await mongoose.connect(env.mongodbUri);
+    await mongoose.connect(mongodbUri);
 
     console.log("✅ MongoDB Connected");
+    console.log(
+      `📦 Connected database: ${mongoose.connection.name}`,
+    );
   } catch (error) {
-    console.error("❌ MongoDB Connection Failed");
-    console.error(error);
-
+    console.error("❌ MongoDB Connection Failed:", error);
     process.exit(1);
   }
 }
