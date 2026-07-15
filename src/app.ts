ed569483914f818c1af0f075f472e2830537b54b
Md Nur Hossain Riyad <md_nur_hossain.riyad@smail.th-koeln.de>
2026-07-15 21:42:00 +0600

    solve cors problem

--- src/app.ts
+++ src/app.ts
@@ -16,7 +16,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:3000",
-      "https://YOUR-FRONTEND-VERCEL-URL.vercel.app",
+      "https://edu-spark-client.vercel.app",
     ],
     credentials: true,
   }),
